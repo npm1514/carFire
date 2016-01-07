0a413669dd4949fdd02e81101c8b2d62f4d62545
Nick Marucci <npm1514@gmail.com>
2016-01-06 17:29:27 -0700

    day 1 progress

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-angular.module("carFire",['ui.router']).config(function ($urlRouterProvider, $stateProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'home/homeTemp.html',
-      controller: 'homeCtrl'
-    })
-    .state("cars", {
-      url: "/cars",
-      templateUrl: "cars/carsTemp.html",
-      controller: 'carsCtrl'
-    })
-    .state("car", {
-      url: "/cars/:carId",
-      templateUrl: "car/carTemp.html",
-      controller: 'carCtrl'
-    });
+var app = angular.module("carFire", ['firebase','ui.router']);
 
-  $urlRouterProvider
-    .otherwise('/');
+app.constant('firebaseRoot', 'https://carfiremarucci.firebaseio.com');
+
+app.config(function($urlRouterProvider, $stateProvider){
+  $stateProvider
+    .state('cars', {
+      url: '/',
+      templateUrl: 'cars/carsTemp.html'
+    }).state('car', {
+      url: '/cars/:carId',
+      templateUrl: 'car/carTemp.html'
+    });
+  $urlRouterProvider.otherwise('/');
 });
--- mainCtrl.js
+++ mainCtrl.js
@@ -1,20 +1,11 @@
-angular.module("carFire").controller("mainCtrl", function($scope) {
-  $scope.cars = [{year:"2012",
-                  make: "Chevy",
-                  model: "Volt",
-                  mileage: "25,000",
-                  price: "$15,000",
-                  },
-                  {year:"2008",
-                  make: "Toyota",
-                  model: "Tundra",
-                  mileage: "125,000",
-                  price: "$12,000"
-                  },
-                  {year:"2000",
-                  make: "Pontiac",
-                  model: "G6",
-                  mileage: "225,000",
-                  price: "$4,000"
-                  }];
+app.controller("mainCtrl", function($scope, $firebaseObject, $firebaseArray, $firebaseAuth, firebaseRoot) {
+  var carsRef = new Firebase(firebaseRoot + '/cars');
+  var cars = $firebaseArray(carsRef);
+  $scope.cars = cars;
+  $scope.addCar = function(car) {
+    $scope.cars.$add(car);
+  };
+  $scope.getCar = function(carId) {
+    $scope.carId = cars.car;
+  };
 });
